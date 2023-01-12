74f8aaf126f563004ef0e2a1ae712c8ea05a35cc
Yihui Xie <xie@yihui.name>
2023-01-11 22:12:17 -0600

    tweak error message

--- release.R
+++ release.R
@@ -2,6 +2,6 @@ install.packages('xfun', repos = 'https://yihui.r-universe.dev')
 
 pkgs = xfun:::cran_updatable(30, 'Yihui Xie')
 pkgs = setdiff(pkgs, 'rolldown')  # ignore rolldown
-if (length(pkgs) > 0) stop(
-  paste(sprintf('https://cran.rstudio.com/package=%s', pkgs), collapse = '\n')
-)
+if (length(pkgs) > 0) stop(paste(c(
+  'Packages need to be updated:', sprintf('https://cran.rstudio.com/package=%s', pkgs)
+), collapse = '\n'))
