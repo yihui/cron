531178984ee26f00b7ee374690a642bd44fe531f
Yihui Xie <xie@yihui.name>
2023-03-06 14:39:52 -0600

    include package names on the first line of the error message so I can easily preview the list in GHA

--- release.R
+++ release.R
@@ -3,5 +3,6 @@ install.packages('xfun', repos = 'https://yihui.r-universe.dev')
 pkgs = xfun:::cran_updatable(30, 'Yihui Xie')
 pkgs = setdiff(pkgs, 'rolldown')  # ignore rolldown
 if (length(pkgs) > 0) stop(paste(c(
-  'Packages need to be updated:', sprintf('https://cran.rstudio.com/package=%s', pkgs)
+  paste(c('Packages need to be updated:', pkgs), collapse = ' '),
+  sprintf('https://cran.rstudio.com/package=%s', pkgs)
 ), collapse = '\n'))
