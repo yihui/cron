544d5da1b4e143f5c5f896b4ee75473b869853b2
Yihui Xie <xie@yihui.name>
2025-01-06 09:29:30 -0600

    make it possible to release a specific package

--- release.R
+++ release.R
@@ -1,8 +1,28 @@
 install.packages('xfun', repos = 'https://yihui.r-universe.dev')
 
-pkgs = xfun:::cran_updatable(30, 'Yihui Xie')
-pkgs = setdiff(pkgs, 'rolldown')  # ignore rolldown
-if (length(pkgs) > 0) stop(paste(c(
-  paste(c('Packages need to be updated:', pkgs), collapse = ' '),
-  sprintf('https://cran.rstudio.com/package=%s', pkgs)
-), collapse = '\n'))
+if (file.exists(f <- 'package/DESCRIPTION')) {
+  # release this package
+  xfun:::install_deps(dirname(f))
+  x = xfun::read_utf8(f)
+  i = grep('^Version: ', x)
+  v = sub('^Version: (\\d+\\.\\d+).*', '\\1', x[i])
+  v = as.integer(strsplit(v, '.', fixed = TRUE)[[1]])
+  v[2] = v[2] + 1
+  v = paste(v, collapse = '.')
+  x[i] = paste('Version:', v)
+  xfun::write_utf8(x, f)
+  p = xfun:::pkg_build(dirname(f))
+  # when CRAN submission is closed, keep trying
+  resp = curlGetHeaders('https://xmpalantir.wu.ac.at/cransubmit/index2.php')
+  if (any(startsWith(resp, 'Location: index.php?strErr=1'))) {
+    xfun::retry(xfun::submit_cran, p, .times = 30, .pause = 600)
+  } else xfun::submit_cran(p)
+} else {
+  # check which packages are too old and need new releases
+  pkgs = xfun:::cran_updatable(30, 'Yihui Xie')
+  pkgs = setdiff(pkgs, 'rolldown')  # ignore rolldown
+  if (length(pkgs) > 0) stop(paste(c(
+    paste(c('Packages need to be updated:', pkgs), collapse = ' '),
+    sprintf('https://cloud.r-project.org/package=%s', pkgs)
+  ), collapse = '\n'))
+}
